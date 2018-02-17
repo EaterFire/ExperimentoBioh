180c7c14fa1f43e6d9ca7e0322b7552fd5c6d7c4
Yago Theodoro <freonzx@gmail.com>
2018-02-17 06:47:55 -0200

    fixed untracked files

---
+++ Assets/Game/Script/Plataforma.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataforma : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter2D	(Collision2D colisor)
+	{
+		if (colisor.gameObject.tag == "Poço")
+			Destroy (gameObject);
+
+	}
+	void OnMouseOver()
+	{
+		if(Input.GetMouseButtonDown(0)){
+			Destroy(gameObject);
+		
+		}
+}
+}
